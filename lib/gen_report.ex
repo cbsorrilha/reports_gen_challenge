1e1e2c556dcd4fc1d2e9fb83d83675a97800ef71
Cesar de Barros <cesar.de.b.sorrilha@accenture.com>
2021-05-21 23:25:46 -0300

    finish paralelism challenge

--- lib/gen_report.ex
+++ lib/gen_report.ex
@@ -17,6 +17,46 @@ defmodule GenReport do
     |> Enum.reduce(@base_acc, fn line, report -> calculate(line, report) end)
   end
 
+  def build_from_many, do: {:error, "Insira uma lista de nomes de arquivos"}
+
+  def build_from_many(filenames) when not is_list(filenames) do
+    {:error, "Insira uma lista de nomes de arquivos"}
+  end
+
+  def build_from_many(filenames) do
+    filenames
+    |> Task.async_stream(&build/1)
+    |> Enum.reduce(@base_acc, fn {:ok, result}, report -> sum_reports(report, result) end)
+  end
+
+  defp sum_reports(
+         %{
+           "all_hours" => all_hours1,
+           "hours_per_month" => per_month1,
+           "hours_per_year" => per_year1
+         },
+         %{
+           "all_hours" => all_hours2,
+           "hours_per_month" => per_month2,
+           "hours_per_year" => per_year2
+         }
+       ) do
+    all_hours = merge_maps(all_hours1, all_hours2)
+    per_month = merge_maps(per_month1, per_month2)
+    per_year = merge_maps(per_year1, per_year2)
+    build_report(all_hours, per_month, per_year)
+  end
+
+  defp merge_maps(map1, map2) do
+    Map.merge(map1, map2, fn _key, value1, value2 -> sum_map_values(value1, value2) end)
+  end
+
+  defp sum_map_values(value1, value2) when is_map(value1) and is_map(value2) do
+    merge_maps(value1, value2)
+  end
+
+  defp sum_map_values(value1, value2), do: value1 + value2
+
   defp calculate([name, hours, _day, month, year], report) do
     all_hours = report["all_hours"]
     per_month = report["hours_per_month"]
@@ -49,5 +89,13 @@ defmodule GenReport do
       Map.put(current_user_years, year, hours + Map.get(current_user_years, year, 0))
 
     Map.put(per_year, name, new_user_years)
+  end
+
+  defp build_report(all_hours, per_month, per_year) do
+    %{
+      "all_hours" => all_hours,
+      "hours_per_month" => per_month,
+      "hours_per_year" => per_year
+    }
   end
 end
--- test/gen_report_test.exs
+++ test/gen_report_test.exs
@@ -5,6 +5,7 @@ defmodule GenReportTest do
   alias GenReport.Support.ReportFixture
 
   @file_name "gen_report.csv"
+  @file_name_list ["part_1.csv", "part_2.csv", "part_3.csv"]
 
   describe "build/1" do
     test "When passing file name return a report" do
@@ -17,6 +18,20 @@ defmodule GenReportTest do
       response = GenReport.build()
 
       assert response == {:error, "Insira o nome de um arquivo"}
+    end
+  end
+
+  describe "build_from_many/1" do
+    test "When passing file names list return a report" do
+      response = GenReport.build_from_many(@file_name_list)
+      # Repeating the same test to guarantee it's returning the same report
+      assert response == ReportFixture.build()
+    end
+
+    test "When the given param is not a filenames list, returns an error" do
+      response = GenReport.build_from_many()
+
+      assert response == {:error, "Insira uma lista de nomes de arquivos"}
     end
   end
 end
